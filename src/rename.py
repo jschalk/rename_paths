709d6db454616b1b67e32b03672afd6af6100c30
Joachim <jschalk@users.noreply.github.com>
2024-06-14 16:03:43 -0700

    fix bugs found through ui

--- src/rename.py
+++ src/rename.py
@@ -30,11 +30,14 @@ def rename_files_and_folders(
                 lower_old_string = old_string.lower()
                 lower_new_dir_name = d.replace(lower_old_string, lower_new_string)
                 lower_new_dir_path = os_path_join(root, lower_new_dir_name)
-                if old_dir_path != lower_new_dir_path:
+                if (
+                    old_dir_path != lower_new_dir_path
+                    and old_file_path.lower() == lower_new_file_path
+                ):
                     alert_messages.append("huh")
 
         # List of file extensions to consider
-        file_extensions = [".py", ".json"]
+        file_extensions = [".py", ".json", ".ui"]
         # Rename files
         for file_name in files:
             if any(file_name.endswith(ext) for ext in file_extensions):
@@ -43,6 +46,7 @@ def rename_files_and_folders(
                 new_file_path = os_path_join(root, new_file_name)
                 if old_file_path != new_file_path:
                     os_rename(old_file_path, new_file_path)
+                    print(f"{old_string=} {new_string=} {new_file_path=}")
                 else:
                     lower_new_string = new_string.lower()
                     lower_old_string = old_string.lower()
@@ -51,8 +55,10 @@ def rename_files_and_folders(
                         lower_old_string, lower_new_string
                     )
                     lower_new_file_path = os_path_join(root, lower_new_file_name)
-                    print(f"{old_file_path=} {lower_new_file_path=}")
-                    if old_file_path != lower_new_file_path:
+                    if (
+                        old_file_path != lower_new_file_path
+                        and old_file_path.lower() == lower_new_file_path
+                    ):
                         alert_messages.append(f"File that was not renamed: {file_name}")
 
     for alert_message in alert_messages:
